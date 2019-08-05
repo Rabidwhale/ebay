2d23cf57ce49a03a1f0748356cd31d8e50fcfd4f
Michael Williams <michaelwilliamsdvc15@gmail.com>
2019-08-05 21:57:28 +0000

    more comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :prod
+
+  RATINGS = {
+    'One Star': '1_Star',
+    'Two Stars': '2_Stars',
+    'Three Stars': '3_Stars',
+    'Four Stars': '4_Stars',
+    'Five Stars': '5_Stars'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end

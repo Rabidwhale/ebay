e670f4ed5ebcf5fdcccc52a680ea44717f17f623
Michael Williams <michaelwilliamsdvc15@gmail.com>
2019-08-05 21:42:04 +0000

    more comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @prod = Prod.find_by_id(params[:prod_id])
+    @prod.comments.create(comment_params.merge(user: current_user))
+    redirect_to prod_path(@prod)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message, :rating)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :categories
   devise_for :users
   root "static_pages#index"
-  resources :prods
+  resources :prods do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
